56acad46025e0252ec0e09915bad77b75200b906
andychan516 <623247195@qq.com>
2021-03-22 18:06:52 +0800

    Initial commit

---
+++ nacos-provider/src/main/java/com/lele/nacosprovider/NacosProviderApplication.java
@@ -0,0 +1,15 @@
+package com.lele.nacosprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient //必须有，
+public class NacosProviderApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(NacosProviderApplication.class, args);
+  }
+
+}
